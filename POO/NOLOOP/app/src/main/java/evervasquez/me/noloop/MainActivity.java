bb91db4d4fe86298dd00aaef1b64eecd868bad58
eveR Vasquez <pever@unsm.edu.pe>
2015-11-04 10:50:32 -0500

    clase de revision de LPOO

---
+++ POO/NOLOOP/app/src/main/java/evervasquez/me/noloop/MainActivity.java
@@ -0,0 +1,79 @@
+package evervasquez.me.noloop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import java.net.InterfaceAddress;
+
+import evervasquez.me.noloop.Abstract.AlumnoSistemas;
+import evervasquez.me.noloop.NoAbstract.Gato;
+import evervasquez.me.noloop.NoAbstract.Perro;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Perro perro;
+    private InterfaceAddress interfaceAddress
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        perro = new Perro();
+        Gato gato = new Gato();
+        Log.v("Perro", perro.dondeHagoLaAccion());
+        Log.v("Gato", gato.dondeHagoLaAccion());
+
+        //example 2
+        AlumnoSistemas alumnoSistemas = new AlumnoSistemas(20);
+        Log.v("El alumno Antony", alumnoSistemas.dias()+"");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("Gato", perro.dondeHagoLaAccion());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
